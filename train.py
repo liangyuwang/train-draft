10e0db86aa750b83ec71c7e65155ac200d4c28e0
liangyuwang <ly.wang.2022@outlook.com>
2025-09-14 16:21:08 +0800

    polish code

--- train.py
+++ train.py
@@ -38,7 +38,7 @@ class TrainerConfig:
     log_dir = "./log/"
     dataset_path = "../data/fineweb-edu-sample-10BT/"
     use_mock_data = False
-    mock_dataset_len = 10
+    mock_data_num_samples = 128
     tokenizer_name = "gpt2"
     total_batch_size = 524288 # 2**19, ~0.5M, in number of tokens, range 0.5~4M, usually 1~2M
     B = 8 # micro batch size per device
@@ -90,7 +90,7 @@ class Trainer:
                     x = torch.randint(0, self.vocab_size, (self.seq_len,), dtype=torch.long)
                     y = torch.randint(0, self.vocab_size, (self.seq_len,), dtype=torch.long)
                     return {"input_ids": x, "labels": y}
-            self.train_dataset = MockDataset(config.mock_dataset_len, config.T)
+            self.train_dataset = MockDataset(config.mock_data_num_samples, config.T)
             train_sampler = DistributedSampler(
                 self.train_dataset, num_replicas=self.dp_world_size, rank=self.dp_rank, shuffle=True
             )
@@ -102,7 +102,7 @@ class Trainer:
                 pin_memory=True,
             )
             if config.do_val:
-                self.val_dataset = MockDataset(config.mock_dataset_len // 10, config.T)
+                self.val_dataset = MockDataset(config.mock_data_num_samples // 10, config.T)
                 val_sampler = DistributedSampler(
                     self.val_dataset, num_replicas=self.dp_world_size, rank=self.dp_rank, shuffle=False
                 )
