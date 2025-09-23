01a123f44bc1dc964b5a5db916e4d9268b958e9c
liangyuwang <ly.wang.2022@outlook.com>
2025-09-23 13:39:37 +0800

    fix test bugs

--- test/moe/grouped_gemm.py
+++ test/moe/grouped_gemm.py
@@ -4,6 +4,9 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
 
 import time
 from typing import Tuple
